9983d3aed137448ebc52c2e6fe72ffeab96f47c9
jergauth <jeremiegthr@gmail.com>
2019-02-16 11:55:18 +0100

    ex

---
+++ deploy_script.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+if [ $# -eq 0 ]; then
+
+	echo "Usage:"
+	echo "	./roger-deploy \"Message de Commit"\"
+
+	exit 1
+fi
+
+if [ $# -eq 1 ]; then
+	git add .
+	git commit -m "$1"
+	git push
+	echo "Git repository updated. Now running update on server."
+	ssh jergauth@192.168.56.3 -p 2222 "/home/jergauth/update_server.sh"
+	exit 0
+fi
+
+exit 1
